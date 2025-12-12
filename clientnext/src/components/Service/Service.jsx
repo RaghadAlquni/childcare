f10199305c62798ae4da9bb8d0d19ea9dd858121
Raghad Alquni <raghadalquni@gmail.com>
2025-12-12 20:52:48 +0300

    fix

--- clientnext/src/components/Service/Service.jsx
+++ clientnext/src/components/Service/Service.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import ServiceStyles from "./Service.module.css"
 
 const servicesCards = [
